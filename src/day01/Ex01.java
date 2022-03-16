45ce431a0ede5bf19a39126579fe52177fc89d9f
Kichan <ckcc21@gmail.com>
2022-03-16 11:33:18 +0900

    Ex01 print

---
+++ src/day01/Ex01.java
@@ -0,0 +1,22 @@
+package day01;
+// package : 그냥 폴더라고 생각하면 된다.
+public class Ex01 {
+// public, class : 키워드라고 생각하면 된다.
+// Ex01을 여기서 바꾸면 안되고 파일명을 바꿔줘야 한다.
+// Package Explorer에서 Ex01.java 클릭 후 F2
+	public static void main(String[] args) { // {}는 범위
+	// main + ctrl + 스페이스바 : 자동 완성 기능(직접 치는 것을 추천)
+	// main : 프로그램의 시작점이라고 생각하면 된다.
+		System.out.println("안녕하세요!!!"); // ""는 문자열의 의미를 갖고 있다.
+		System.out.println("자바시간입니다"); // ctrl + alt + 방향키 아래 : 그대로 복사
+		// System.out.println : 괄호 안의 출력하는 기능
+		System.out.print("출력");
+		System.out.print("합니다");
+		// print는 엔터가 없음
+		
+		// 한줄 주석
+		/*
+		 여러줄 주석
+		 */
+	}
+}
